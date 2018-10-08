a0be626add16afd223f00bd87bd297bbe2afb78f
Pavan Kumar Majeti <pavan1979@gmail.com>
2018-10-08 11:26:27 +0530

    Avoid Multiple Original Service calls and Cache the Response

---
+++ src/app/cacheInterceptor.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpResponse, HttpInterceptor, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/do';
+import { HttpCacheService } from './cache.service';
+
+@Injectable()
+export class CacheInterceptor implements HttpInterceptor {
+    constructor(private cacheService: HttpCacheService) { }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        console.log('this is cache interceptor');
+
+        if (req.url.indexOf('/LoginCustomer') < 0) {
+            const cachedResponse = this.cacheService[req.urlWithParams] || null;
+
+            if (cachedResponse) {
+                console.log('response from cache');
+                return Observable.of(cachedResponse);
+            }
+        }
+
+        return next.handle(req).do(event => {
+            if (event instanceof HttpResponse) {
+                this.cacheService[req.urlWithParams] = event;
+                console.log('response from server');
+            }
+        });
+    }
+}
--- src/app/login/loginservice.ts
+++ src/app/login/loginservice.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestMethod, ResponseContentType } from '@angular/http';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import 'rxjs/Rx';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Rx';
@@ -8,26 +8,23 @@ import { Subject } from 'rxjs/Rx';
 export class LoginService {
     private notify = new Subject<any>();
     notifyObservable$ = this.notify.asObservable();
-  constructor(private http:HttpClient) { }
-  
-postdetails(url:string,userData:any)
-{
-  
- return this.http.post(url,userData).map((response:Response)=>
-{
-    if (userData) {
-        this.notify.next(userData);
-      }
-return response;
+    constructor(private http: HttpClient) { }
 
-}).catch((error:Response)=>
-{
- return Observable.throw(error);
-});
-}
+    postdetails(url: string, userData: any) {
+
+        return this.http.post(url, userData).map((response: Response) => {
+            if (userData) {
+                this.notify.next(userData);
+            }
+            return response;
+
+        }).catch((error: Response) => {
+            return Observable.throw(error);
+        });
+    }
 
 
-logout(): void {
-    this.notify.next('Sign In');
-}
+    logout(): void {
+        this.notify.next('Sign In');
+    }
 }
