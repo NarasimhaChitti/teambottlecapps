fe65f8d302dad101a77720d2f9233b553b258748
Pavan Kumar Majeti <pavan1979@gmail.com>
2018-10-08 19:07:41 +0530

    Restricted API Calls if no SessionID and AppID

--- src/app/cacheInterceptor.ts
+++ src/app/cacheInterceptor.ts
@@ -12,15 +12,23 @@ export class CacheInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log('this is cache interceptor');
 
-        if (req.url.indexOf('/LoginCustomer') < 0) {
-            const cachedResponse = this.cacheService[req.urlWithParams] || null;
+        // if (req.url.indexOf('/LoginCustomer') < 0) {}
+        const cachedResponse = this.cacheService[req.urlWithParams] || null;
 
-            if (cachedResponse) {
-                console.log('response from cache');
-                return Observable.of(cachedResponse);
-            }
+        if (cachedResponse) {
+            console.log('response from cache');
+            return Observable.of(cachedResponse);
         }
 
+        if (req.url.indexOf('/StoreGetHome') > 0) {
+            if (!req.body.SessionId) {
+                return;
+            }
+        } else {
+            if (!req.body.AppId) {
+                return;
+            }
+        }
         return next.handle(req).do(event => {
             if (event instanceof HttpResponse) {
                 this.cacheService[req.urlWithParams] = event;
